3960a4e8466dd864f575036f0954be53e5c1d6b8
kailyl <kliu279@gmail.com>
2022-04-30 20:11:11 -0400

    add mapping from years to list of senators

--- src/WikiParser.java
+++ src/WikiParser.java
@@ -5,24 +5,29 @@ import org.jsoup.nodes.TextNode;
 import org.jsoup.select.Elements;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WikiParser {
     private String baseURL;
     private Document currentDoc;
+    private Map<String, String> senatorsToYear;
+    private Map<String, List<String>> yearToSenator;
 
     public WikiParser() {
         this.baseURL = "https://en.wikipedia.org/wiki/List_of_United_States_senators_in_the_117th_Congress";
         try {
             this.currentDoc = Jsoup.connect(this.baseURL).get();
+            this.senatorsToYear = new HashMap<>();
+            this.yearToSenator = new HashMap<>();
         } catch (IOException e) {
             System.out.println("Could not get the site :(");
         }
     }
 
-    public Map<String, String> getTableFillMap() {
-        Map<String, String> senators = new HashMap<>();
+    public Map<String, String> getSenatorsToYear() {
         Elements articleElements = this.currentDoc.select("table[class*=wikitable sortable]");
         Elements rows = articleElements.select("tbody");
         Elements senateAttr = rows.select("td");
@@ -33,20 +38,41 @@ public class WikiParser {
             if (elem.childNodeSize() >= 2 && elem.childNode(0).hasAttr("data-sort-value")) {
                 currSenator = elem.childNode(0).attr("data-sort-value");
                 if (currYear != null) {
-                    String[] str = currYear.split(" ");
-                    senators.put(currSenator, str[2]);
+                    putInHashMaps(currYear, currSenator);
                 }
             }
             if (elem.childNodeSize() == 1) {
                 TextNode n = (TextNode) elem.childNode(0);
                 if (n.text().matches(".*[0-9].*") && n.text().matches(".*,.*")) {
                     currYear = n.text();
-                    String[] str = currYear.split(" ");
-                    senators.put(currSenator, str[2]);
+                    putInHashMaps(currYear, currSenator);
                 }
             }
         }
-        return senators;
+        System.out.println(yearToSenator);
+        return senatorsToYear;
+    }
+
+    private void putInHashMaps(String currYear, String currSenator) {
+        String[] str = currYear.split(" ");
+        senatorsToYear.put(currSenator, str[2]);
+        if (yearToSenator.containsKey(str[2])) {
+            containsSenator(yearToSenator, currSenator);
+            yearToSenator.get(str[2]).add(currSenator);
+        } else {
+            containsSenator(yearToSenator, currSenator);
+            List<String> list = new ArrayList<>();
+            list.add(currSenator);
+            yearToSenator.put(str[2], list);
+        }
+    }
+
+    private void containsSenator(Map<String, List<String>> map, String senator) {
+        for (String key : map.keySet()) {
+            if (map.get(key).contains(senator)) {
+                map.get(key).remove(senator);
+            }
+        }
     }
 }
 
--- src/WikiParserMain.java
+++ src/WikiParserMain.java
@@ -4,7 +4,7 @@ public class WikiParserMain {
 
     public static void main (String[] args) {
         WikiParser parse = new WikiParser();
-        Map<String, String> map = parse.getTableFillMap();
+        Map<String, String> map = parse.getSenatorsToYear();
         System.out.print(map);
     }
 }
